4291a968cdb439486f50650f6ec88a94091c8912
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-24 10:59:03 +0700

    add body-parser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
+
+app.use(bodyParser.json())
 
 const port = 3000
 
